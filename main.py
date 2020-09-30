3fcdb145624d806569fe7c234cb203cb5ea77744
Kirill Sulim <kirillsulim@gmail.com>
2020-09-30 14:45:47 +0500

    Add english version

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
+from shutil import copy
 from typing import List
 
 from pylatex import Document, Section, Subsection
 from pylatex.utils import italic
+
+from jinja2 import Environment
 
 from model import Data, load_data
 
@@ -13,13 +16,18 @@ class Runner:
     def __init__(self, args: List[str]):
         parser = ArgumentParser()
         parser.add_argument("--data-file", type=Path, default=Path("./data.yaml"))
+        parser.add_argument("--lang", choices=["ru", "en"], default="ru")
 
         args = parser.parse_args(args)
 
         self.data_file = args.data_file
+        self.lang = args.lang
+        self.build_dir = Path("./build")
 
     def run(self) -> int:
         data = load_data(self.data_file)
+
+        self._render_html(data)
 
         return 0
 
@@ -34,6 +42,21 @@ class Runner:
                 doc.append('Also some crazy characters: $&#{}')
 
         doc.generate_pdf('cv.pdf')
+
+    def _render_html(self, data: Data):
+        env = Environment()
+        template = env.from_string(Path("./resources/html/index.html").read_text())
+        rendered = template.render(data=data, lang=self.lang, job_title="Java developer")
+
+        html_dir = self.build_dir / "html"
+        html_dir.mkdir(exist_ok=True, parents=True)
+        copy(Path("./resources/html/style.css"), html_dir / "style.css")
+        copy(Path("./resources/html/sulim.jpg"), html_dir / "sulim.jpg")
+
+        html_rendered = html_dir / "index.html"
+        html_rendered.write_text(rendered)
+
+        stop = True
 
 
 if __name__ == '__main__':
